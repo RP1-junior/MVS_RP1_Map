3b11ac006f8867f0a1e1abb296510f98bb23b428
junior <work+rp1@juniorbustamante.com>
2025-09-19 10:53:59 -0700

    changes

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-const { MVSF, CreateMVSQL } = require('@metaversalcorp/mvsf');
+const { MVSF } = require('@metaversalcorp/mvsf');
 const { InitSQL } = require('./utils.js');
 const Settings = require('./settings.json');
 const { MVSQL_MYSQL } = require('@metaversalcorp/mvsql_mysql');
 const fs = require('fs');
 const path = require('path');
+const http = require('http');
 
-/*******************************************************************************************************************************
-**                                                     Main                                                                   **
-*******************************************************************************************************************************/
 class MVSF_Map {
   #pServer;
   #pSQL;
@@ -31,35 +29,41 @@ class MVSF_Map {
 
   onSQLReady(pMVSQL, err) {
     if (pMVSQL) {
-      // Use Railway's dynamic port
       Settings.MVSF.nPort = process.env.PORT || 3000;
 
-      // Start MVSF server
+      // Create the MVSF server instance (but don't let it listen itself)
       this.#pServer = new MVSF(Settings.MVSF, require('./handler.json'), __dirname, null, 'application/json');
-
-      // Add route to serve fabric.msf
-      this.#pServer.AddRoute('/fabric.msf', (req, res) => {
-        const filePath = path.join(__dirname, 'fabric.msf');
-        fs.readFile(filePath, (err, data) => {
-          if (err) {
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.end('File not found');
-          } else {
-            res.writeHead(200, { 'Content-Type': 'application/octet-stream' });
-            res.end(data);
-          }
-        });
-      });
-
-      this.#pServer.Run();
 
       console.log('SQL Server READY');
       InitSQL(pMVSQL, this.#pServer, Settings.Info);
+
+      // Create a single HTTP server that handles both /fabric.msf and MVSF
+      const server = http.createServer((req, res) => {
+        if (req.url === '/fabric.msf') {
+          const filePath = path.join(__dirname, 'fabric.msf');
+          fs.readFile(filePath, (err, data) => {
+            if (err) {
+              res.writeHead(404, { 'Content-Type': 'text/plain' });
+              res.end('File not found');
+            } else {
+              res.writeHead(200, { 'Content-Type': 'application/octet-stream' });
+              res.end(data);
+            }
+          });
+        } else {
+          // Let MVSF handle all other requests
+          this.#pServer.OnRequest(req, res);
+        }
+      });
+
+      server.listen(Settings.MVSF.nPort, () => {
+        console.log(`Server running on port ${Settings.MVSF.nPort}`);
+      });
+
     } else {
       console.log('SQL Server Connect Error: ', err);
     }
   }
 }
 
-// Start the app
-const g_pServer = new MVSF_Map();
+new MVSF_Map();
