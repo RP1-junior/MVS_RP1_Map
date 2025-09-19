6604fc0bfe3e0157167c7695410866d75db5b340
junior <work+rp1@juniorbustamante.com>
2025-09-19 11:05:50 -0700

    revert

--- server.js
+++ server.js
@@ -1,69 +1,58 @@
-const { MVSF } = require('@metaversalcorp/mvsf');
-const { InitSQL } = require('./utils.js');
-const Settings = require('./settings.json');
-const { MVSQL_MYSQL } = require('@metaversalcorp/mvsql_mysql');
-const fs = require('fs');
-const path = require('path');
-const http = require('http');
+const { MVSF, CreateMVSQL } = require ('@metaversalcorp/mvsf');
+const { InitSQL      } = require ('./utils.js');
+const Settings = require ('./settings.json');
 
-class MVSF_Map {
-  #pServer;
-  #pSQL;
+// const { MVSQL_MSSQL  } = require ('@metaversalcorp/mvsql_mssql');
+const { MVSQL_MYSQL  } = require ('@metaversalcorp/mvsql_mysql');
 
-  constructor() {
-    switch (Settings.SQL.type) {
-      case 'MYSQL':
-        Settings.SQL.config.host = process.env.MYSQLHOST;
-        Settings.SQL.config.port = process.env.MYSQLPORT;
-        Settings.SQL.config.user = process.env.MYSQLUSER;
-        Settings.SQL.config.password = process.env.MYSQLPASSWORD;
-        Settings.SQL.config.database = process.env.MYSQLDATABASE;
-        console.log('setting: ', Settings.SQL.config);
-        this.#pSQL = new MVSQL_MYSQL(Settings.SQL.config, this.onSQLReady.bind(this));
-        break;
-      default:
-        this.#pSQL = null;
-        break;
-    }
-  }
+/*******************************************************************************************************************************
+**                                                     Main                                                                   **
+*******************************************************************************************************************************/
+class MVSF_Map
+{
+   #pServer;
+   #pRDS;
+   #pSQL;
+//   #pRequire;
 
-  onSQLReady(pMVSQL, err) {
-    if (pMVSQL) {
-      Settings.MVSF.nPort = process.env.PORT || 3000;
+   constructor ()
+   {
+      switch (Settings.SQL.type) {
+//         case 'MSSQL':
+//            this.#pSQL = new MVSQL_MSSQL(Settings.SQL, this.onSQLReady.bind(this));
+//            break;
+         case 'MYSQL':
+               Settings.SQL.config.host= process.env.MYSQLHOST;
+               Settings.SQL.config.port= process.env.MYSQLPORT;
+               Settings.SQL.config.user= process.env.MYSQLUSER;
+               Settings.SQL.config.password= process.env.MYSQLPASSWORD;
+               Settings.SQL.config.database= process.env.MYSQLDATABASE;
+               console.log('setting: ', Settings.SQL.config);
+            this.#pSQL = new MVSQL_MYSQL(Settings.SQL.config, this.onSQLReady.bind(this));
+            break;
+         default:
+            pMVSQL = null;
+            break;
+      }
+   }
 
-      // Create MVSF instance
-      this.#pServer = new MVSF(Settings.MVSF, require('./handler.json'), __dirname, null, 'application/json');
+   onSQLReady (pMVSQL, err)
+   {
+      if (pMVSQL)
+      {
+//         this.#pRequire = MV.MVMF.Core.Require ('MVRP_RDS');
+Settings.MVSF.nPort = process.env.PORT || 3000;
+         this.#pServer = new MVSF (Settings.MVSF, require ('./handler.json'), __dirname, null, 'application/json');
+         this.#pServer.Run ();
 
-      console.log('SQL Server READY');
-      InitSQL(pMVSQL, this.#pServer, Settings.Info);
-
-      // Create a single HTTP server
-      const server = http.createServer((req, res) => {
-        if (req.url === '/fabric.msf') {
-          const filePath = path.join(__dirname, 'fabric.msf');
-          fs.readFile(filePath, (err, data) => {
-            if (err) {
-              res.writeHead(404, { 'Content-Type': 'text/plain' });
-              res.end('File not found');
-            } else {
-              res.writeHead(200, { 'Content-Type': 'application/octet-stream' });
-              res.end(data);
-            }
-          });
-        } else {
-          // Let MVSF handle everything else
-          this.#pServer.ServeHTTP(req, res);
-        }
-      });
-
-      server.listen(Settings.MVSF.nPort, () => {
-        console.log(`Server running on port ${Settings.MVSF.nPort}`);
-      });
-
-    } else {
-      console.log('SQL Server Connect Error: ', err);
-    }
-  }
+         console.log ('SQL Server READY');
+         InitSQL (pMVSQL, this.#pServer, Settings.Info);
+      }
+      else
+      {
+         console.log ('SQL Server Connect Error: ', err);
+      }
+   }
 }
 
-new MVSF_Map();
+const g_pServer = new MVSF_Map ();
