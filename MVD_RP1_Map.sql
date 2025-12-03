6803bae9e365574e07be2e2105ab160a55d6d0ad
junior <work+rp1@juniorbustamante.com>
2025-12-03 14:04:38 -0800

    Update MVD_RP1_Map.sql

--- MVD_RP1_Map.sql
+++ MVD_RP1_Map.sql
@@ -13640,76 +13640,87 @@ END$$
 DELIMITER ;
 
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-/*******************************************************************************************************************************
-**                                                                                                                            **
-**                                             MVD_RP1_Map : init_DefaultScene.sql                                            **
-**                                                                                                                            **
-********************************************************************************************************************************
-**                            Copyright (c) 2023-2025 Metaversal Corporation. All rights reserved.                            **
-*******************************************************************************************************************************/
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~                                                                                                                            ~~
+~~                                             MVD_RP1_Map : init_DefaultScene.sql                                            ~~
+~~                                                                                                                            ~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~                            Copyright (c) 2023-2025 Metaversal Corporation. All rights reserved.                            ~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-CALL set_RMRoot_RMPObject_Open
+DELIMITER $$
+
+CREATE PROCEDURE init_DefaultScene
 (
-   '0.0.0.0',                 -- sIPAddress           
-   1,                         -- twRPersonaIx         
-   1,                         -- twRMRootIx           
-   'My First Scene',          -- Name_wsRMPObjectId   
-   1,                         -- Type_bType           
-   0,                         -- Type_bSubtype        
-   1,                         -- Type_bFiction        
-   0,                         -- Type_bMovable        
-   1,                         -- Owner_twRPersonaIx   
-   0,                         -- Resource_qwResource  
-   '',                        -- Resource_sName       
-   '',                        -- Resource_sReference  
-   0,                         -- Transform_Position_dX
-   0,                         -- Transform_Position_dY
-   0,                         -- Transform_Position_dZ
-   0,                         -- Transform_Rotation_dX
-   0,                         -- Transform_Rotation_dY
-   0,                         -- Transform_Rotation_dZ
-   1,                         -- Transform_Rotation_dW
-   1,                         -- Transform_Scale_dX   
-   1,                         -- Transform_Scale_dY   
-   1,                         -- Transform_Scale_dZ   
-   150,                       -- Bound_dX             
-   150,                       -- Bound_dY             
-   150,                       -- Bound_dZ             
-   NULL                       -- nResult
-);
--- twRMPObjectIx = 1
+)
+BEGIN
+       DECLARE nResult INT;
 
+       CALL set_RMRoot_RMPObject_Open
+       (
+          '0.0.0.0',                 -- sIPAddress           
+          1,                         -- twRPersonaIx         
+          1,                         -- twRMRootIx           
+          'My First Scene',          -- Name_wsRMPObjectId   
+          1,                         -- Type_bType           
+          0,                         -- Type_bSubtype        
+          1,                         -- Type_bFiction        
+          0,                         -- Type_bMovable        
+          1,                         -- Owner_twRPersonaIx   
+          0,                         -- Resource_qwResource  
+          '',                        -- Resource_sName       
+          '',                        -- Resource_sReference  
+          0,                         -- Transform_Position_dX
+          0,                         -- Transform_Position_dY
+          0,                         -- Transform_Position_dZ
+          0,                         -- Transform_Rotation_dX
+          0,                         -- Transform_Rotation_dY
+          0,                         -- Transform_Rotation_dZ
+          1,                         -- Transform_Rotation_dW
+          1,                         -- Transform_Scale_dX   
+          1,                         -- Transform_Scale_dY   
+          1,                         -- Transform_Scale_dZ   
+          150,                       -- Bound_dX             
+          150,                       -- Bound_dY             
+          150,                       -- Bound_dZ             
+          nResult                    -- nResult
+       );
+       -- twRMPObjectIx = 1
 
-CALL set_RMPObject_RMPObject_Open
-(
-   '0.0.0.0',                 -- sIPAddress           
-   1,                         -- twRPersonaIx         
-   1,                         -- twRMPObjectIx        
-   'Hello World!',            -- Name_wsRMPObjectId   
-   1,                         -- Type_bType           
-   0,                         -- Type_bSubtype        
-   1,                         -- Type_bFiction        
-   0,                         -- Type_bMovable        
-   1,                         -- Owner_twRPersonaIx   
-   0,                         -- Resource_qwResource  
-   '',                        -- Resource_sName       
-   '/scenes/hello_world.glb', -- Resource_sReference  
-   0,                         -- Transform_Position_dX
-   0,                         -- Transform_Position_dY
-   0,                         -- Transform_Position_dZ
-   0,                         -- Transform_Rotation_dX
-   0,                         -- Transform_Rotation_dY
-   0,                         -- Transform_Rotation_dZ
-   1,                         -- Transform_Rotation_dW
-   1,                         -- Transform_Scale_dX   
-   1,                         -- Transform_Scale_dY   
-   1,                         -- Transform_Scale_dZ   
-   134.65382385253906,        -- Bound_dX             
-   13.596150933846705,        -- Bound_dY             
-   129.60743890149325,        -- Bound_dZ             
-   NULL                       -- nResult
-);
--- twRMPObjectIx = 2
+       CALL set_RMPObject_RMPObject_Open
+       (
+          '0.0.0.0',                 -- sIPAddress           
+          1,                         -- twRPersonaIx         
+          1,                         -- twRMPObjectIx        
+          'Hello World!',            -- Name_wsRMPObjectId   
+          1,                         -- Type_bType           
+          0,                         -- Type_bSubtype        
+          1,                         -- Type_bFiction        
+          0,                         -- Type_bMovable        
+          1,                         -- Owner_twRPersonaIx   
+          0,                         -- Resource_qwResource  
+          '',                        -- Resource_sName       
+          '/scenes/hello_world.glb', -- Resource_sReference  
+          0,                         -- Transform_Position_dX
+          0,                         -- Transform_Position_dY
+          0,                         -- Transform_Position_dZ
+          0,                         -- Transform_Rotation_dX
+          0,                         -- Transform_Rotation_dY
+          0,                         -- Transform_Rotation_dZ
+          1,                         -- Transform_Rotation_dW
+          1,                         -- Transform_Scale_dX   
+          1,                         -- Transform_Scale_dY   
+          1,                         -- Transform_Scale_dZ   
+          134.65382385253906,        -- Bound_dX             
+          13.596150933846705,        -- Bound_dY             
+          129.60743890149325,        -- Bound_dZ             
+          nResult                    -- nResult
+       );
+       -- twRMPObjectIx = 2
+END$$
+  
+DELIMITER ;
 
+CALL init_DefaultScene ();
 
-/******************************************************************************************************************************/
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
